00b48daf567a12e22d64160e31c20bcb5b90a7fd
Benjamin Rottler <benjamin@dierottlers.de>
2025-08-20 18:28:30 +0200

    Update Talos to 1.10.6

--- main.tf
+++ main.tf
@@ -42,8 +42,8 @@ module "talos" {
     dns_servers                     = ["1.1.1.1", "1.0.0.1"]
     gateway                         = "192.168.30.1"
     bridge                          = "vmbr1"
-    talos_version                   = "1.10.3"
-    talos_update_version            = "1.10.3"
+    talos_version                   = "1.10.6"
+    talos_update_version            = "1.10.6"
     kubernetes_version              = "1.33.1"
     kubernetes_api_endpoint_url     = "talos-prod.home.rottlr.de"
     kubernetes_api_vip              = "192.168.30.50"
